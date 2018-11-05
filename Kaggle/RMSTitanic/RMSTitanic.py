b5a0b36c30f0e8b4317c0648a1216f3d27156115
buiqtuan <tuanbq2909@gmail.com>
2018-11-06 00:19:19 +0700

    Done visualizing data

--- Kaggle/RMSTitanic/RMSTitanic.py
+++ Kaggle/RMSTitanic/RMSTitanic.py
@@ -6,7 +6,7 @@ import numpy as np
 import pandas as pd
 
 # data visualization
-# import seaborn as sns
+import seaborn as sns
 from matplotlib import pyplot as plt
 from matplotlib import style
 
@@ -51,13 +51,67 @@ if gettrace():
 test_df = pd.read_csv(testFilePath)
 train_df = pd.read_csv(trainFilePath)
 
+#----------------- VISUALIZING DATA -----------------
 # print(train_df.info())
 # print(train_df.describe())
 # print(train_df.head(15))
 
 # Let's take a more detailed look at what data is actually missing:
-total = train_df.isnull().sum().sort_values(ascending=False)
-percent_1 = train_df.isnull().sum()/train_df.isnull().count()*100
-percent_2 = (round(percent_1, 1)).sort_values(ascending=False)
-missing_data = pd.concat([total, percent_2], axis=1, keys=['Total', '%'])
-print(missing_data.head(5))
+# total = train_df.isnull().sum().sort_values(ascending=False)
+# percent_1 = train_df.isnull().sum()/train_df.isnull().count()*100
+# percent_2 = (round(percent_1, 1)).sort_values(ascending=False)
+# missing_data = pd.concat([total, percent_2], axis=1, keys=['Total', '%'])
+# print out the most 5 features have the highest rates of missing values
+# print(missing_data.head(5))
+
+# print(train_df.columns.values)
+
+#----------------- ANALyZING DATA -----------------
+# 1. Sex and Age
+# survived = 'survived'
+# not_survived = 'not_survived'
+# fig, axes = plt.subplots(nrows=1, ncols=2, figsize=(10,4))
+# women = train_df[train_df['Sex'] == 'female']
+# men = train_df[train_df['Sex'] == 'male']
+
+# ax = sns.distplot(women[women['Survived'] == 1].Age.dropna(),
+#                 bins=18, label=survived, ax=axes[0], kde=False)
+# ax = sns.distplot(women[women['Survived'] == 0].Age.dropna(),
+#                 bins=40, label=not_survived, ax=axes[0], kde=False)
+# ax.legend()
+# ax.set_title('Female')
+
+# ax = sns.distplot(men[men['Survived'] == 1].Age.dropna(),
+#                 bins=18, label=survived, ax=axes[1], kde=False)
+# ax = sns.distplot(men[men['Survived'] == 0].Age.dropna(),
+#                 bins=40, label=not_survived, ax=axes[1], kde=False)
+# ax.legend()
+# ax.set_title('Male')
+
+# plt.show()
+
+# 2. Embarked, Pclass and Sex:
+# FacetGrid = sns.FacetGrid(train_df, row='Embarked', size=4.5, aspect=1.6)
+# FacetGrid.map(sns.pointplot, 'Pclass', 'Survived', 'Sex', palette=None, order=None, hue_order=None)
+# FacetGrid.add_legend()
+
+# Pclass 
+# sns.barplot(x='Pclass', y='Survived', data=train_df)
+
+# Relation between Age and Pclass
+# grid = sns.FacetGrid(train_df, col='Survived', row='Pclass', size=2.2, aspect=1.6)
+# grid.map(plt.hist, 'Age', alpha=.5, bins=20)
+# grid.add_legend()
+
+# 3. SibSp and Parch
+_data = [train_df, test_df]
+for dataset in _data:
+    dataset['relatives'] = dataset['SibSp'] + dataset['Parch']
+    dataset.loc[dataset['relatives'] > 0, 'not_alone'] = 0
+    dataset.loc[dataset['relatives'] == 0, 'not_alone'] = 1
+    dataset['not_alone'] = dataset['not_alone'].astype(int)
+
+print(train_df['not_alone'].value_counts())
+
+axes = sns.factorplot('relatives', 'Survived', data=train_df, aspect=2.5)
+plt.show()
