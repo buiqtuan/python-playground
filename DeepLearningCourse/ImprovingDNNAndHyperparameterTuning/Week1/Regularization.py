cf9504c0118fbea4cacc95729bec5efc1f5fb024
buiqtuan <tuanbq2909@gmail.com>
2018-11-25 23:06:36 +0700

    Init Assignment 2 - Course 1 - Part 4 - W1

---
+++ DeepLearningCourse/ImprovingDNNAndHyperparameterTuning/Week1/Regularization.py
@@ -0,0 +1,14 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from reg_utils import *
+import sklearn
+import sklearn.datasets
+import scipy.io
+from testCases import *
+
+plt.rcParams['figure.figsize'] = (7.0, 4.0) # set default size of plots
+plt.rcParams['image.interpolation'] = 'nearest'
+plt.rcParams['image.cmap'] = 'gray'
+
+train_X, train_Y, test_X, test_Y = load_2D_dataset()
+
