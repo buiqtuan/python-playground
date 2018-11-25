98748ea4d07be5205ef194d21aa4fe6cb6bc69a0
buiqtuan <tuanbq2909@gmail.com>
2018-11-25 13:47:35 +0700

    Done Course 1 Week 4 Assignment

--- DeepLearningCourse/NeuralNetAndDeelLearning/Week4/DNNApplication.py
+++ DeepLearningCourse/NeuralNetAndDeelLearning/Week4/DNNApplication.py
@@ -124,8 +124,66 @@ def two_layer_model(X, Y, layers_dims, learning_rate = 0.0075, num_iterations = 
 
 	return parameters
 
-parameters = two_layer_model(train_x, train_y, layers_dims = (n_x, n_h, n_y), num_iterations = 2500, print_cost = True)
+# parameters = two_layer_model(train_x, train_y, layers_dims = (n_x, n_h, n_y), num_iterations = 2500, print_cost = True)
 
-prediction_train = predict(train_x, train_y, parameters)
+# prediction_train = predict(train_x, train_y, parameters)
 
-prediction_test = predict(test_x, test_y, parameters)
+# prediction_test = predict(test_x, test_y, parameters)
+
+layers_dims = [12288, 20, 7, 5, 1] # 4 layers model
+
+def L_layer_model(X, Y, layers_dims, learning_rate = 0.0075, num_iterations = 3000, print_cost = False):
+    """
+    Implements a L-layer neural network: [LINEAR->RELU]*(L-1)->LINEAR->SIGMOID.
+    
+    Arguments:
+    X -- data, numpy array of shape (number of examples, num_px * num_px * 3)
+    Y -- true "label" vector (containing 0 if cat, 1 if non-cat), of shape (1, number of examples)
+    layers_dims -- list containing the input size and each layer size, of length (number of layers + 1).
+    learning_rate -- learning rate of the gradient descent update rule
+    num_iterations -- number of iterations of the optimization loop
+    print_cost -- if True, it prints the cost every 100 steps
+    
+    Returns:
+    parameters -- parameters learnt by the model. They can then be used to predict.
+    """
+    np.random.seed(1)
+    costs = []
+
+    # Parameters initialization
+    parameters = initialize_parameters_deep(layers_dims)
+
+    # Loop 
+    for i in range(0, num_iterations):
+        # Forward propagation: [LINEAR -> RELU]*(L-1) -> LINEAR -> SIGMOID.
+        AL, caches = L_model_forward(X, parameters)
+
+        # Compute cost
+        cost = compute_cost(AL, Y)
+
+        # Backware propagation
+        grads = L_model_backward(AL, Y, caches)
+
+        # Update parameters
+        parameters = update_parameters(parameters, grads, learning_rate)
+
+        if print_cost and i % 100 == 0:
+            print("Cost after iteration %i: %f"%(i, cost))
+            costs.append(cost)
+
+    # Plot the cost
+    plt.plot(np.squeeze(costs))
+    plt.ylabel('Cost')
+    plt.xlabel('Iteration (per tens)')
+    plt.title("Learning rate = " + str(learning_rate))
+    plt.show()
+
+    return parameters
+
+parameters = L_layer_model(train_x, train_y, layers_dims, num_iterations=2500, print_cost=True)
+
+pred_train = predict(train_x, train_y, parameters)
+print("Training accuracy: ", pred_train)
+
+pred_test = predict(test_x, test_y, parameters)
+print("Testing accuracy: ", pred_test)
