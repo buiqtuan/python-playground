00e2dd99bb64460ff1081d544a7f812a946e84b8
buiqtuan <tuanbq2909@gmail.com>
2018-12-11 00:52:47 +0700

    done back-propa, max-pool back, average-pool back

--- DeepLearningCourse/ConvolutinonalNeuralNetworks/Week1/ConvolutionalModel.py
+++ DeepLearningCourse/ConvolutinonalNeuralNetworks/Week1/ConvolutionalModel.py
@@ -136,10 +136,189 @@ np.random.seed(1)
 A_prev = np.random.randn(10,4,4,3)
 W = np.random.randn(2,2,3,8)
 b = np.random.randn(1,1,1,8)
-hparameters = {"pad" : 2,
-               "stride": 2}
+hparameters = {"pad" : 2, "stride": 2}
 
 Z, cache_conv = conv_forward(A_prev, W, b, hparameters)
-print("Z's mean =", np.mean(Z))
-print("Z[3,2,1] =", Z[3,2,1])
-print("cache_conv[0][1][2][3] =", cache_conv[0][1][2][3])
+# print("Z's mean =", np.mean(Z))
+# print("Z[3,2,1] =", Z[3,2,1])
+# print("cache_conv[0][1][2][3] =", cache_conv[0][1][2][3])
+
+def pool_forward(A_prev, hparameters, mode='max'):
+    """
+    Implements the forward pass of the pooling layer
+    
+    Arguments:
+    A_prev -- Input data, numpy array of shape (m, n_H_prev, n_W_prev, n_C_prev)
+    hparameters -- python dictionary containing "f" and "stride"
+    mode -- the pooling mode you would like to use, defined as a string ("max" or "average")
+    
+    Returns:
+    A -- output of the pool layer, a numpy array of shape (m, n_H, n_W, n_C)
+    cache -- cache used in the backward pass of the pooling layer, contains the input and hparameters
+    """
+
+    # Retrieve demensions from the input shape
+    (m, n_H_prev, n_W_prev, n_C_prev) = A_prev.shape
+
+    # Retrieve hyperparameters from 'hparameters'
+    f = hparameters['f']
+    stride = hparameters['stride']
+    
+    # Define the demensions of the output
+    n_H = int(1 + (n_H_prev - f)/stride)
+    n_W = int(1 + (n_W_prev - f)/stride)
+    n_C = n_C_prev
+
+    A = np.zeros((m, n_H, n_W, n_C))
+
+    for i in range(m):
+        for h in range(n_H):
+            for w in range(n_W):
+                for c in range(n_C):
+                    vert_start = h
+                    vert_end = h + f
+                    horiz_start = w
+                    horiz_end = w + f
+
+                    a_prev_slice = A_prev[i][vert_start:vert_end][horiz_start:horiz_end][c]
+
+                    if mode == 'max':
+                        A[i, h, w, c] = np.max(a_prev_slice)
+                    elif mode == 'average':
+                        A[i, h, w, c] = np.mean(a_prev_slice)
+
+    # Store the input and hparameters in 'cache' for pool_backward()
+    cache = (A_prev, hparameters)
+
+    # Making sure the output shape is correct
+    assert(A.shape == (m, n_H, n_W, n_C))
+
+    return A, cache
+
+# np.random.seed(1)
+# A_prev = np.random.randn(2, 4, 4, 3)
+# hparameters = {"stride" : 2, "f": 3}
+
+# A, cache = pool_forward(A_prev, hparameters)
+# print("mode = max")
+# print("A =", A)
+# print()
+# A, cache = pool_forward(A_prev, hparameters, mode = "average")
+# print("mode = average")
+# print("A =", A)
+
+def conv_backward(dZ, cache):
+    """
+    Implement the backward propagation for a convolution function
+    
+    Arguments:
+    dZ -- gradient of the cost with respect to the output of the conv layer (Z), numpy array of shape (m, n_H, n_W, n_C)
+    cache -- cache of values needed for the conv_backward(), output of conv_forward()
+    
+    Returns:
+    dA_prev -- gradient of the cost with respect to the input of the conv layer (A_prev),
+               numpy array of shape (m, n_H_prev, n_W_prev, n_C_prev)
+    dW -- gradient of the cost with respect to the weights of the conv layer (W)
+          numpy array of shape (f, f, n_C_prev, n_C)
+    db -- gradient of the cost with respect to the biases of the conv layer (b)
+          numpy array of shape (1, 1, 1, n_C)
+    """
+    # Retrieve information from 'cache'
+    (A_prev, W, b, hparameters) = cache
+
+    # Retrieve dimensions from A_prev's shape
+    (m, n_H_prev, n_W_prev, n_C_prev) = A_prev.shape
+
+    # Retrieve dimensions from W's shape
+    (f, f, n_C_prev, n_C) = W.shape
+
+    # Retrieve information from 'hparameters'
+    stride = hparameters['stride']
+    pad = hparameters['pad']
+
+    # Retrieve dimensions from dZ's shape
+    (m, n_H, n_W, n_C) = dZ.shape
+
+    # Initialize dA_prev, dW, db with the correct shapes
+    dA_prev = np.zeros((m, n_H_prev, n_W_prev, n_C_prev))
+    dW = np.zeros((f, f, n_C_prev, n_C))
+    db = np.zeros((1, 1, 1, n_C))
+
+    # Pad A_prev and dA_prev
+    A_prev_pad = zero_pad(A_prev, pad)
+    dA_prev_pad = zero_pad(dA_prev, pad)
+
+    for i in range(m):
+        a_prev_pad = A_prev_pad[i]
+        da_prev_pad = dA_prev_pad[i]
+        for h in range(n_H):
+            for w in range(n_W):
+                for c in range(n_C):
+                    vert_start = h
+                    vert_end = h + f
+                    horiz_start = w
+                    horiz_end = w + f
+
+                    a_slice = a_prev_pad[vert_start:vert_end, horiz_start:horiz_end,:]
+
+                    da_prev_pad[vert_start:vert_end, horiz_start:horiz_end,:] += W[...,c]*dZ[i, h, w, c]
+                    dW[...,c] += W[...,c]
+                    db[...,c] += b[...,c]
+
+        # Set the ith training example's dA_prev to the unpaded da_prev
+        dA_prev[i,:,:,:] = da_prev_pad[pad:-pad, pad:-pad, :]
+
+    assert(dA_prev.shape == (m, n_H_prev, n_W_prev, n_C_prev))
+
+    return dA_prev, dW, db
+
+np.random.seed(1)
+dA, dW, db = conv_backward(Z, cache_conv)
+print("dA_mean =", np.mean(dA))
+print("dW_mean =", np.mean(dW))
+print("db_mean =", np.mean(db))
+
+def create_mask_from_window(x):
+    """
+    Creates a mask from an input matrix x, to identify the max entry of x.
+    
+    Arguments:
+    x -- Array of shape (f, f)
+    
+    Returns:
+    mask -- Array of the same shape as window, contains a True at the position corresponding to the max entry of x.
+    """
+    k = np.max(x)
+
+    mask = np.array([[False if (i < k) else True for i in j] for j in x])
+
+    return mask
+
+# np.random.seed(1)
+# x = np.random.randn(2,3)
+# mask = create_mask_from_window(x)
+# print('x = ', x)
+# print("mask = ", mask)
+
+def distribute_value(dz, shape):
+    """
+    Distributes the input value in the matrix of dimension shape
+    
+    Arguments:
+    dz -- input scalar
+    shape -- the shape (n_H, n_W) of the output matrix for which we want to distribute the value of dz
+    
+    Returns:
+    a -- Array of size (n_H, n_W) for which we distributed the value of dz
+    """
+
+    (n_H, n_W) = shape
+
+    average = float(dz/(n_H*n_W))
+
+    a = np.array([[average for i in range(n_H)] for j in range(n_H)])
+
+    return a
+
+# a = distribute_value(2, (2,2))
+# print('distributed value =', a)
